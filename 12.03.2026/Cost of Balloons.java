52b6cb5b38e113265a1486a047985182a5917262
Vaitheeshwaran <vaitheeshwaran010506@gmail.com>
2026-03-13 06:41:20 +0530

    Create Cost of Balloons.java

---
+++ 12.03.2026/Cost of Balloons.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+class TestClass {
+    public static void main(String args[] ) throws Exception {
+        Scanner sc=new Scanner(System.in);
+        int T=sc.nextInt();
+        while(T-->0){
+
+        int gc=sc.nextInt();
+        int pc=sc.nextInt();
+
+        int p=sc.nextInt();
+int c1=0;
+int c2=0;
+
+for(int i=0;i<p;i++){
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+
+    if(a==1){
+        c1++;
+    }
+    if(b==1){
+        c2++;
+    }
+}
+
+int cst1 = c1 * gc + c2 * pc;
+            int cst2 = c1 * pc+ c2 * gc;
+
+            System.out.println(Math.min(cst1, cst2));
+
+}
+    }
+}
