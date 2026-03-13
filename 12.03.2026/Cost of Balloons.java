c7e6bfd954eea05b54598c7587e1b92d581ba2af
Vaitheeshwaran <vaitheeshwaran010506@gmail.com>
2026-03-13 22:52:00 +0530

    Update Cost of Balloons.java

--- 12.03.2026/Cost of Balloons.java
+++ 12.03.2026/Cost of Balloons.java
@@ -26,10 +26,10 @@ for(int i=0;i<p;i++){
     }
 }
 
-int cst1 = c1 * gc + c2 * pc;
+int cost1 = c1 * gc + c2 * pc;
             int cst2 = c1 * pc+ c2 * gc;
 
-            System.out.println(Math.min(cst1, cst2));
+            System.out.println(Math.min(cost1, cst2));
 
 }
     }
